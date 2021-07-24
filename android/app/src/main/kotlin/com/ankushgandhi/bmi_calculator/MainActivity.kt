0ab26e4f3daf8f116241db6263584fd0b501f8ba
ankush <bhanusinghank@gmail.com>
2021-07-24 19:23:49 +0530

    initialized project

---
+++ android/app/src/main/kotlin/com/ankushgandhi/bmi_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.ankushgandhi.bmi_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: InputPage(),
+    );
+  }
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+      ),
+      body: Center(
+        child: Text('jimmy'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          print('hi');
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
